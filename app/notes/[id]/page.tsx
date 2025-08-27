863492e70e14cd16af056313eb24f3c919623a53
Tetiana <Korostijtatiana@gmail.com>
2025-08-27 20:52:10 +0300

    client

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,7 +3,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NoteDetailsClient from "./NoteDetails.client";
+import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
 import { fetchNoteById } from "@/lib/api";
 
 interface NoteDetailsProps {
