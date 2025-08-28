38b5ed8a6f1e5a5a261615018eba26e5cca5397b
Tetiana <Korostijtatiana@gmail.com>
2025-08-28 16:34:26 +0300

    add  refetchOnMount

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -12,6 +12,7 @@ export default function NoteDetailsClient({ id }: { id: string }) {
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     enabled: !!id,
+    refetchOnMount: false,
   });
 
   if (isLoading) return <p>Loading, please wait...</p>;
